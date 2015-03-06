d928847549211bd141686be831da741b20ad91b6
Simao Belchior <simaobelchior@gmail.com>
2015-03-06 17:06:24 +0000

    Adding edition of common names

--- app/scripts/views/edition_window_view.js
+++ app/scripts/views/edition_window_view.js
@@ -16,7 +16,9 @@ define([
       'click .modal-background': 'closeInfowindow',
       'submit #update-description': 'updateDescription',
       'submit #update-distribution': 'updateDistribution',
-      'click #add-new-dist': 'addNewDist'
+      'click #add-new-dist': 'addNewDist',
+      'submit #update-common-names': 'updateCommonNames',
+      'click #add-new-common': 'addNewCommon'
     },
 
     /**
@@ -72,6 +74,9 @@ define([
       // Add new
       var self = this;
       $.each(e.target.children[0].children, function() {
+        if($(this).hasClass('labels')){
+          return true;
+        }
         var val = this.children[0].value;
         var id;
         if($(this.children[0]).hasClass('new-item')){
@@ -114,6 +119,72 @@ define([
       var $el = $('.m-existing-item').last().clone();
       $el.find('input').attr('id','').val('');
       $el.find('input').addClass('new-item');
+      $('.existing-items-list').append($el);
+    },
+
+    updateCommonNames: function(e) {
+      e.preventDefault();
+      var url = 'http://ec2-54-94-97-96.sa-east-1.compute.amazonaws.com:8282' +
+        '/api/commonNames';
+      var speciesId = this.model.attributes.speciesId;
+
+      // Add new
+      var self = this;
+      $.each(e.target.children[0].children, function() {
+        if($(this).hasClass('labels')){
+          return true;
+        }
+        var name = this.children[0].value;
+        var language = this.children[1].value;
+        var region = this.children[2].value;
+        var id;
+        if($(this).hasClass('new-item')){
+          id = null;
+        } else {
+          id = parseInt(this.id.replace('common-name-', ''));
+          var item = _.findWhere(self.model.attributes.commonNames,
+                                 {id: id});
+          item['name'] = name;
+          item['language'] = language;
+          item['region'] = region;
+        }
+        $.ajax({
+          url: url,
+          type: 'PUT',
+          data: JSON.stringify({
+            id: id,
+            region: region,
+            speciesId: speciesId,
+            language: language,
+            name: name
+          }),
+          contentType: 'application/json',
+          dataType: 'json',
+          success: function(data) {
+            if(!_.findWhere(self.model.attributes.commonNames,
+                            {id: data.id})){
+              self.model.attributes.commonNames.push({
+                id: data.id,
+                speciesId: data.speciesId,
+                region: data.region,
+                language: data.language,
+                name: data.name
+              });
+              self.trigger('editionWindowView:recordSaved');
+            }
+          }
+        });
+      });
+      this.trigger('editionWindowView:recordSaved');
+      this.closeInfowindow();
+    },
+
+    addNewCommon: function(e) {
+      e.preventDefault();
+      var $el = $('.m-existing-item').last().clone();
+      $el.attr('id','');
+      $el.addClass('new-item');
+      $el.find('.input').val('');
       $('.existing-items-list').append($el);
     }
 
