3fcbd17f1cbf5ca436f8be8b067aeef6110de9f4
Simao Belchior <simaobelchior@gmail.com>
2015-03-06 14:35:06 +0000

    grrr simao

--- app/scripts/collections/species_collection.js
+++ app/scripts/collections/species_collection.js
@@ -2,7 +2,7 @@ define(['backbone'], function(Backbone) {
 
   'use strict';
 
-  var apiDomain = 'localhost:8282';
+  var apiDomain = 'http://ec2-54-94-97-96.sa-east-1.compute.amazonaws.com:8282';
   var apiUrl = apiDomain + '/api/taxa';
 
   var SpeciesCollection = Backbone.Collection.extend({
